7fb4dd4c4661aaf16b99dac43061cfe152c7e21c
pablodamascenoo <pablofacex@gmail.com>
2022-10-23 14:37:24 -0300

    feat: added launchRouter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import launchRouter from "./launchRouter.js";
 
 const router = Router()
+
+router.use(launchRouter)
 
 export default router
---
+++ src/routes/launchRouter.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import launchController from "../controllers/launchController.js";
+
+const launchRouter = Router()
+
+launchRouter
+.get("/past", launchController.getPast)
+.get("/latest", launchController.getLatest)
+.get("/next", launchController.getNext)
+.get("/upcoming", launchController.getUpcoming)
+
+export default launchRouter
