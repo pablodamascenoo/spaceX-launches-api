bd71e6d84845730ee81b7982dd504340a50b7bc1
pablodamascenoo <pablofacex@gmail.com>
2022-10-23 14:36:37 -0300

    feat: added errorHandler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
-import express, { json } from "express"
-import cors from "cors"
-import router from "./routes/index.js"
+import "express-async-errors";
+import express, { json } from "express";
+import cors from "cors";
+import router from "./routes/index.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
-const app = express()
-app.use(json())
-app.use(cors())
-app.use(router)
+const app = express();
+app.use(json());
+app.use(cors());
+app.use(router);
+app.use(errorHandler);
 
-export default app
+export default app;
